b7e94108e510188aa34d724aacdd9ae30657b100
吴烜 <wuxuan.ecios@gmail.com>
2021-05-17 22:20:24 -0700

    0.7.9 准备发布

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.rst") as f:
     readme = f.read()
 
 setup(
-    name="rply",
-    description="A pure Python Lex/Yacc that works with RPython",
+    name="rply-ulang",
+    description="A pure Python Lex/Yacc that works with RPython，木兰编程语言定制版",
     long_description=readme,
     # duplicated in docs/conf.py and rply/__init__.py
-    version="0.7.7",
-    author="Alex Gaynor",
-    author_email="alex.gaynor@gmail.com",
+    version="0.7.9",
+    author="Xuan Wu",
+    author_email="mulanrevive@gmail.com",
     packages=["rply"],
     install_requires=["appdirs"],
 )
